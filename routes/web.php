bc6aaa1ea3ee9a3571d18ec3195cbd2bb9998d50
Rizaldy Putra <nyxzryu@gmail.com>
2018-11-25 21:58:36 +0700

    Upload set route, controller, model adminDashboard, add admin template for layouts

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,4 @@ Route::get('/sign-in', 'HomeController@signin');
 Route::get('/chat', 'HomeController@chat');
 Route::get('/success', 'HomeController@success');
 Route::post('/appointment/create', 'SendPetController@store');
+Route::resource('/indexAdmin', 'AdminModelController');
